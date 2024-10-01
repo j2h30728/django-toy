3e8ee8c8149314eef98539dd938c921af1b35409
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-01 12:51:18 +0900

    feat: admin 패널의 검색과 필터 기능 개선

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ $ python manage.py runserver
 
 $ python manage.py makemigrations
 $ python manage.py migrate
+
+
+$ python manage.py createsuperuser
 ```
--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from .models import Like, Tweet
 
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = "Elon Musk"
+    parameter_name = "elon_musk"
+
+    def lookups(self, request, model_admin):
+        return [(True, "Contain 'Elon Musk'"), (False, f"Don't contain 'Elon Musk'")]
+
+    def queryset(self, request, queryset):
+        isContainElonMusk = self.value()
+        ELON_MUSK = "Elon Musk"
+        if isContainElonMusk == "True":
+            return queryset.filter(payload__contains=ELON_MUSK)
+        elif isContainElonMusk == "False":
+            return queryset.exclude(payload__contains=ELON_MUSK)
+        else:
+            queryset
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     fieldsets = (
@@ -20,7 +38,14 @@ class TweetAdmin(admin.ModelAdmin):
             },
         ),
     )
-
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+    list_filter = (
+        ElonMuskFilter,
+        "created_at",
+    )
     list_display = (
         "id",
         "user",
@@ -48,6 +73,7 @@ class LikeAdmin(admin.ModelAdmin):
             },
         ),
     )
-
+    search_fields = ("user__username",)
+    list_filter = ("created_at",)
     list_display = ("id", "user", "tweet", "created_at")
     readonly_fields = ("id", "created_at", "updated_at")
